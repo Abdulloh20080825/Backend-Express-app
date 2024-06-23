1532ab2277b729e80cc8d0542dca7a79c19f93fd
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-06-23 14:38:34 +0500

    Dynamic title

--- routes/products.js
+++ routes/products.js
@@ -2,13 +2,21 @@ import { Router } from "express";
 
 const router = Router();
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Boom Shop",
+  });
 });
 router.get("/products", (req, res) => {
-  res.render("products");
+  res.render("products", {
+    title: "Products",
+    isProduct: true,
+  });
 });
 router.get("/add", (req, res) => {
-  res.render("add");
+  res.render("add", {
+    title: "Add Product",
+    isAdd: true,
+  });
 });
 
 export default router;
