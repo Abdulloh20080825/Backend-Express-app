0694365d3461ce4852af324a1a77246ad73a86f6
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-06-23 14:22:22 +0500

    Express Moddleware Routes

--- app.js
+++ app.js
@@ -2,28 +2,22 @@ import express from "express";
 import { engine, create } from "express-handlebars";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import AuthRoutes from "./routes/auth.js";
+import ProductsRoutes from "./routes/products.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 
-const hbs = create({
-  defaultLayout: "main",
-  extname: "hbs",
-});
+const hbs = create({ defaultLayout: "main", extname: "hbs" });
 
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./views");
 
-app.get("/", (req, res) => {  
-  res.render("index");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About page");
-});
+app.use(AuthRoutes);
+app.use(ProductsRoutes);
 
 const PORT = 8080 || process.env.PORT;
 
