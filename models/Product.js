0be3af5657d894ef5bbf3b5d02e8fec06c526ec6
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-07-02 21:10:37 +0500

    Add Product Model MongoDB

---
+++ models/Product.js
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+
+const ProductSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+const Product = model("Product", ProductSchema);
+export default Product;
