e9585a254da749aa0202e5703bd2e1c0e613438d
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-07-04 10:09:37 +0500

    Main page

--- routes/products.js
+++ routes/products.js
@@ -5,9 +5,12 @@ import userMiddleware from "../middleware/user.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const products = await Product.find().lean()
+  console.log(products);
   res.render("index", {
     title: "Boom Shop",
+    products: products
   });
 });
 
