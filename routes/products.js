10d9fec63c4d5eec8be9f0088952d7781c1f702d
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-07-04 22:42:20 +0500

    My Products

--- routes/products.js
+++ routes/products.js
@@ -14,10 +14,14 @@ router.get("/", async (req, res) => {
   });
 });
 
-router.get("/products", (req, res) => {
+router.get("/products", async (req, res) => {
+  const user = req.userId ? req.userId.toString() : null;
+  const myProducts = await Product.find({ user }).populate("user").lean();
+  console.log(myProducts);
   res.render("products", {
     title: "Products",
     isProduct: true,
+    myProducts: myProducts,
   });
 });
 
